947f1d1d2aaecf5a17eee66079ac9896d6ffe5b5
Nigel Cleland <nigel.cleland@gmail.com>
2013-04-18 15:07:35 +1200

    Changed to init.py to improve module handling

--- nzem/__init__.py
+++ nzem/__init__.py
@@ -1,5 +1,5 @@
 from nzem.masks import apply_masks
-from nzem.data_import import load_reserve_prices
+from nzem.data_import import *
 from nzem.offer_curves import *
 from nzem.constraints import *
 from nzem.offer_analysis import *
