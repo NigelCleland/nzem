245d9606265a7cf47cccb6591e3fb168819aa9b2
NigelCleland <nigel.cleland@gmail.com>
2013-04-19 14:51:00 +1200

    Added helper function to load the inflow data for analysis

--- nzem/hydrology.py
+++ nzem/hydrology.py
@@ -7,9 +7,21 @@ import glob
 import nzem
 from dateutil.parser import parse
 
-def load_hydro_data(csv_name=None, parse_dates=True, date_index=True):
+def load_hydro_data(csv_name=None, date="Trading Date", parse_dates=True,
+                    date_index=True):
     """
-    Load the 
+    Load the hydrology data with optional date and index handling.
+    
+    Parameters
+    ----------
+    csv_name : Name of the file to be loaded as hydro data
+    date : String containing the trading date name
+    parse_dates : Boolean, whether to attempt to parse the dates
+    date_index : Boolean, should the date be assigned to the index
+    
+    Returns
+    -------
+    lake_storage : DataFrame containing the lake storage information
     """    
     
     if csv_name == None:
@@ -17,13 +29,45 @@ def load_hydro_data(csv_name=None, parse_dates=True, date_index=True):
     
     lake_storage = pd.read_csv(csv_name)
     if parse_dates:
-        lake_storage["Trading Date"] = lake_storage["Trading Date"].apply(
-                lambda x: parse(x))
+        lake_storage[date] = lake_storage[date].apply(lambda x: parse(x))
     if date_index:
-        lake_storage.index = lake_storage["Trading Date"]
+        lake_storage.index = lake_storage[date]
         
-    lake_storage = lake_storage.sort("Trading Date")
+    lake_storage = lake_storage.sort(date)
     return lake_storage
+    
+    
+def load_inflow_data(csv_name=None, date="Trading Date", parse_dates=True,
+                     date_index=True):
+    """
+    Helper function to load the inflow data with optional initial data
+    manipulation arguments
+    
+    Parameters
+    ----------
+    csv_name : Name of the csv file to be loaded, if None defaults to the
+               repo file
+    date : Name of the trading date column
+    parse_dates : Boolean, whether to parse the trading dates
+    date_index : Boolean, Should the date be assigned to an index
+    
+    Returns
+    -------
+    inflow_levels : DataFrame containing lake storage level information
+    """
+    
+    if csv_name == None:
+        csv_name = os.get_cwd() + '/nzem/maps/Hydro_Inflow_Data.csv"
+        
+    inflow_levels = pd.read_csv(csv_name)
+    if parse_dates:
+        inflow_levels[date] = inflow_levels[date].apply(lambda x: parse(x))
+    if date_index:
+        inflow_levels.index = inflow_levels[date]
+    
+    inflow_levels = inflow_levels.sort(date)
+    return inflow_levels
+
     
 
 
