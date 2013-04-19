f25a23967eded8b560a35e425d5f169b62deccb0
NigelCleland <nigel.cleland@gmail.com>
2013-04-19 14:33:52 +1200

    Added more helper function masks
    Added some more masks to provide syntactic sugar for pandas DataFrames.
    These masks make it easier to provide filters for various boolean operations
    on columns which may be chained together.
    
    I should probably move these masks to a separate repo and develop them stand alone
    in order that they may be of use to others

--- nzem/masks.py
+++ nzem/masks.py
@@ -4,6 +4,8 @@ easier splitting and indexing of data
 """
 
 import pandas as pd
+import numpy as np
+from itertools import izip
 
 def eq_mask(df, key, value):
     return df[df[key] == value]
@@ -26,6 +28,26 @@ def ne_mask(df, key, value):
 def gen_mask(df, f):
     return df[f(df)]
     
+def in_eqmask(df, key, values):
+    l = (df.eq_mask(key, value) for value in values)
+    return pd.concat(l).drop_duplicates()
+    
+def mix_eqmask(df, keys, values):
+    l = (df.eq_mask(key, value) for key, value in izip(keys, values))
+    return pd.concat(l).drop_duplicates()
+    
+def mixbool_mask(df, keys, bools, values):
+    bool_dict = {"eq": df.eq_mask, "ge": df.ge_mask, "le": df.le_mask,
+                 "lt": df.lt_mask, "gt": df.gt_mask, "ne": df.ne_mask}
+    l = (bool_dict[b](key, value) for b, key, value in izip(bools, keys, values))
+    return pd.concat(l).drop_duplicates()
+    
+def bet_mask(df, key, values, inclusive=True):
+    if inclusive:
+        return df.ge_mask(key, values[0]).le_mask(key, values[1])
+    else:
+        return df.gt_mask(key, values[0]).lt_mask(key, values[1])
+    
 def apply_masks():
     
     pd.DataFrame.eq_mask = eq_mask
@@ -35,6 +57,10 @@ def apply_masks():
     pd.DataFrame.lt_mask = lt_mask
     pd.DataFrame.ne_mask = ne_mask
     pd.DataFrame.gen_mask = gen_mask
+    pd.DataFrame.in_eqmask = in_eqmask
+    pd.DataFrame.mix_eqmask = mix_eqmask
+    pd.DataFrame.mixbool_mask = mixbool_mask
+    pd.DataFrame.bet_mask = bet_mask
     
     return pd.DataFrame
         
