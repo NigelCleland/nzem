a6e835e5868d28cda80e7178174db3c907e8b0df
NigelCleland <nigel.cleland@gmail.com>
2013-04-19 14:02:02 +1200

    Added greater functionality for loading hydrology data

--- nzem/hydrology.py
+++ nzem/hydrology.py
@@ -5,16 +5,24 @@ import sys
 import os
 import glob
 import nzem
+from dateutil.parser import parse
 
-def load_hydro_data(csv_name=None, parse_dates=1):
+def load_hydro_data(csv_name=None, parse_dates=True, date_index=True):
     """
     Load the 
     """    
     
     if csv_name == None:
-        csv_name = './maps/Hydro_lake_Storage.csv'
+        csv_name = os.getcwd() +  '/nzem/maps/Hydro_lake_Storage.csv'
     
-    lake_storage = pd.read_csv(csv_name, parse_dates=parse_dates, dayfirst=True)
+    lake_storage = pd.read_csv(csv_name)
+    if parse_dates:
+        lake_storage["Trading Date"] = lake_storage["Trading Date"].apply(
+                lambda x: parse(x))
+    if date_index:
+        lake_storage.index = lake_storage["Trading Date"]
+        
+    lake_storage = lake_storage.sort("Trading Date")
     return lake_storage
     
 
