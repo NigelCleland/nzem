82858c9db0dee397873484ab32e4f8f325389694
NigelCleland <nigel.cleland@gmail.com>
2013-04-23 13:42:52 +1200

    Added a helper function to merge series and DataFrames together easier

--- nzem/utilities/utilities.py
+++ nzem/utilities/utilities.py
@@ -4,6 +4,9 @@ from datetime import datetime, timedelta
 def niwa_parse(x, parse_str="%d%b%y"):
     d = datetime.strptime(x, parse_str)
     return d if d.year <= 2020 else datetime(d.year - 100, d.month, d.day)
+    
+def merge_series(df, series,**kargs):
+    return df.merge(pd.DataFrame({series.name: series}), right_index=True, **kargs)
 
 if __name__ == '__main__':
     pass
