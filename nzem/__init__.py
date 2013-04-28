0169c8de399d3932b3cda73a91487681648ba429
NigelCleland <nigel.cleland@gmail.com>
2013-04-28 17:20:46 +1200

    Importing the nzem module will now automatically apply the masks to DataFrames and Series

--- nzem/__init__.py
+++ nzem/__init__.py
@@ -7,3 +7,9 @@ from nzem.analysis.hydrology import *
 from nzem.utilities.ts_aggregation import *
 from nzem.utilities.aggregation import *
 from nzem.data_handling.master_set import *
+import pandas as pd
+
+
+# Modifications to be made
+pd.Dataframe = nzem.apply_masks()
+pd.Series = nzem.apply_series_masks()
