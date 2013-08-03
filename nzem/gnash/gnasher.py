7c0bcf8d95a7666c5805744e03aaf114cce57134
Nigel Cleland <nigel.cleland@gmail.com>
2013-08-03 18:35:03 +1200

    Some gnasher bug fixes

--- nzem/gnash/gnasher.py
+++ nzem/gnash/gnasher.py
@@ -14,11 +14,15 @@ import pandas.io.sql as sql
 #import pyodbc
 import os
 import sys
+import subprocess
+from cStringIO import StringIO
 
 if sys.platform.startswith("linux"):
    from pbs import Command
 
 # Change to the gnash directory, assumes it is extracted in the user home path.
+
+cwd = os.getcwd()
 
 gnash_path = os.path.join(os.path.expanduser('~'), 'CDS', 'CentralisedDataset', 'HalfHourly')
 os.chdir(gnash_path)
@@ -26,60 +30,61 @@ os.chdir(gnash_path)
 
 
 class Gnasher(object):
-	"""Gnasher is a class designed to make interfacing with the Gnash.exe as 
-	painless as possible. It should eventually handle constructing queries,
-	returning data as pandas DataFrames and generally taking care of
-	the BS which makes dealing with such systems "fun"
-	"""
-	def __init__(self):
-		super(Gnasher, self).__init__()
-		self._initialise_connection()
+    """Gnasher is a class designed to make interfacing with the Gnash.exe as 
+    painless as possible. It should eventually handle constructing queries,
+    returning data as pandas DataFrames and generally taking care of
+    the BS which makes dealing with such systems "fun"
+    """
+    def __init__(self):
+        super(Gnasher, self).__init__()
+        self._initialise_connection()
 
-	def query_gnash(self, input_string, output_file=None):
-	
-		self._run_query(input_string, output_file=output_file)
-		self.query = self._convertgnashdump(output_file)
-		return self.query
+    def query_gnash(self, input_string, output_file=None):
+    
+        
+        self._run_query(input_string, output_file=output_file)
+        self.query = self._convertgnashdump(output_file)
+        return self.query
 
 
-	def _initialise_connection(self):
-		try:
-			self.gnash = Command("./Gnash.exe")
-		except:
-			print "Error, cannot create a connection to Gnash"
+    def _initialise_connection(self):
+        try:
+            self.gnash = Command("./Gnash.exe")
+        except:
+            print "Error, cannot create a connection to Gnash"
 
-	def _run_query(self, input_string, output_file=None):
-		try:
-			self.gnash(_in=input_string, _out=output_file)
-		except:
-			print "Error, cannot run the query on Gnash"
+    def _run_query(self, input_string, output_file=None):
+        try:
+            return self.gnash(_in=input_string, _out=output_file)
+        except:
+            print "Error, cannot run the query on Gnash"
 
-	
-	def _convertgnashdump(self, output_file):
+    
+    def _convertgnashdump(self, output_file):
 
-		na_conv = lambda x: np.nan if "?" in str(x) else x
+        na_conv = lambda x: np.nan if "?" in str(x) else x
 
-		# First read to obtain dump file
-   		Gin=pd.read_csv(output_file, header=1, skiprows=[2], 
-   			converters={'Aux.DayClock': self._ordinal_converter}, parse_dates=True)
-   		# Dictionary Comprehension to rename columns
-   		new_names = {x: x.replace('.', '_') for x in Gin.columns}
-   		Gin.rename(columns=new_names, inplace=True)
-   		Gin = Gin.applymap(na_conv)
-   		# Set a new index
-		Gin = Gin.set_index('Aux_DayClock')
-   		return Gin
+        # First read to obtain dump file
+        Gin=pd.read_csv(output_file, header=1, skiprows=[2])
+        # Dictionary Comprehension to rename columns
+        new_names = {x: x.replace('.', '_') for x in Gin.columns}
+        Gin.rename(columns=new_names, inplace=True)
+        Gin = Gin.applymap(na_conv)
+        # Set a new index
+        Gin["Aux_DayClock"] = Gin["Aux_DayClock"].apply(self._ordinal_converter)
+        Gin = Gin.set_index('Aux_DayClock')
+        return Gin
 
-	def _ordinal_converter(self, x):
-		if np.isnan(x):
-			return np.nan
-	
-		x =float(x) + datetime.toordinal(datetime(1899,12,31))
-		ord_date = date.fromordinal(int(np.floor(x)))
-		 
-		return datetime(ord_date.year,
-						ord_date.month,
-						ord_date.day,
-						int(np.floor((x % 1.0) * 24)),
-						int(np.round((x % 1.0 * 24 % 1.0 * 60), decimals=0)))
-			
+    def _ordinal_converter(self, x):
+        if np.isnan(x):
+            return np.nan
+    
+        x =float(x) + datetime.toordinal(datetime(1899,12,31))
+        ord_date = date.fromordinal(int(np.floor(x)))
+         
+        return datetime(ord_date.year,
+                        ord_date.month,
+                        ord_date.day,
+                        int(np.floor((x % 1.0) * 24)),
+                        int(np.round((x % 1.0 * 24 % 1.0 * 60), decimals=0)))
+            
