3692194af0029aa7708869ee324fd2044a6d24d9
Nigel Cleland <nigel.cleland@gmail.com>
2013-08-22 21:33:16 +1200

    Updated version number

--- nzem/__init__.py
+++ nzem/__init__.py
@@ -11,4 +11,4 @@ from gnash.gnasher import Gnasher
 from offers.offer_frames import ILOffer, ReserveOffer, EnergyOffer, PLSROffer
 from wits.wits import WitsScraper
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
