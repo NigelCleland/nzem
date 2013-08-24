0fe0ba39c64db913453d481616b547c79aec4d77
Nigel Cleland <nigel.cleland@gmail.com>
2013-08-24 18:25:47 +1200

    Wrapping imports in try statement for docs

--- nzem/__init__.py
+++ nzem/__init__.py
@@ -1,14 +1,18 @@
 # file imports
-import gnash.gnasher
-import offers.offer_frames
-import wits.wits
-import plotting
-import frequent_io
-import analysis
 
-# Class Imports
-from gnash.gnasher import Gnasher
-from offers.offer_frames import ILOffer, ReserveOffer, EnergyOffer, PLSROffer
-from wits.wits import WitsScraper
+try:
+    import gnash.gnasher
+    import offers.offer_frames
+    import wits.wits
+    import plotting
+    import frequent_io
+    import analysis
+
+    # Class Imports
+    from gnash.gnasher import Gnasher
+    from offers.offer_frames import ILOffer, ReserveOffer, EnergyOffer, PLSROffer
+    from wits.wits import WitsScraper
+except:
+    print "Imports failed, please check dependencies"
 
 __version__ = '0.2.1'
