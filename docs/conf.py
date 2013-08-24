50ba5383de80d72031160c3202f71693362eb319
Nigel Cleland <nigel.cleland@gmail.com>
2013-08-24 18:35:01 +1200

    Still trying to get doc imports working

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy', 'pandas', 'matplotlib']
+MOCK_MODULES = ['numpy', 'pandas', 'matplotlib', 'pytz']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
