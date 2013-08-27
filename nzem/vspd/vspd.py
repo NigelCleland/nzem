f2f9087a53a61f5cd91b2602849da0e78395c1d0
Nigel Cleland <nigel.cleland@gmail.com>
2013-08-27 15:38:15 +1200

    Added a vSPUD factory which yields vSPUD objects.
    This factory is intended for iterating over a directory which contains a large
    number of vSPD results. It will compile aggregated DataFrames of the data within
    each sub directory which can then have the vSPUD methods applied to it

--- nzem/vspd/vspd.py
+++ nzem/vspd/vspd.py
@@ -15,18 +15,101 @@ import numpy as np
 
 # Import nzem
 
+class vSPUD_Factory(object):
+    """docstring for ClassName"""
+    def __init__(self, master_folder, pattern=None):
+        super(vSPUD_Factory, self).__init__()
+        self.master_folder = master_folder
+        self.pattern = pattern
+        self.sub_folders = glob.glob(os.path.join(master_folder, '*'))
+        self.sub_folders = [x for x in self.sub_folders if os.path.isdir(x)]
+        if pattern:
+            self.sub_folders = [x for x in self.sub_folders if pattern in x]
+
+
+    def load_results(self, island_results=None, summary_results=None,
+                system_results=None, bus_results=None, reserve_results=None,
+                trader_results=None, offer_results=None, branch_results=None):
+        """
+        Load all of the reserve results
+        """
+
+        if reserve_results:
+            reserve_results = pd.concat((x.reserve_results for
+                x in self._yield_results(reserve=True)),ignore_index=True)
+
+        if island_results:
+            island_results = pd.concat((x.island_results for
+                x in self._yield_results(island=True)),ignore_index=True)
+
+        if summary_results:
+            summary_results = pd.concat((x.summary_results for
+                x in self._yield_results(summary=True)),ignore_index=True)
+
+        if system_results:
+            system_results = pd.concat((x.system_results for
+                x in self._yield_results(system=True)),ignore_index=True)
+
+        if bus_results:
+            bus_results = pd.concat((x.bus_results for
+                x in self._yield_results(bus=True)),ignore_index=True)
+
+        if trader_results:
+            trader_results = pd.concat((x.trader_results for
+                x in self._yield_results(trader=True)),ignore_index=True)
+
+        if offer_results:
+            offer_results = pd.concat((x.offer_results for
+                x in self._yield_results(offer=True)),ignore_index=True)
+
+        if branch_results:
+            branch_results = pd.concat((x.branch_results for
+                x in self._yield_results(branch=True)),ignore_index=True)
+
+        return vSPUD(reserve_results=reserve_results,
+                island_results=island_results, summary_results=summary_results,
+                system_results=system_results, bus_results=bus_results,
+                trader_results=trader_results, offer_results=offer_results,
+                branch_results=branch_results)
+
+
+    def _yield_results(self, **kargs):
+
+        for folder in self.sub_folders:
+            yield vSPUD(folder, **kargs)
+
+
+
 class vSPUD(object):
     """docstring for vSPUD"""
-    def __init__(self, folder):
+    def __init__(self, folder=None, island_results=None, summary_results=None,
+                system_results=None, bus_results=None, reserve_results=None,
+                trader_results=None, offer_results=None, branch_results=None,
+                 **kargs):
         super(vSPUD, self).__init__()
 
-        self.folder = folder
-        self._load_data()
+        self.island_results = island_results
+        self.summary_results = summary_results
+        self.system_results = system_results
+        self.bus_results = bus_results
+        self.reserve_results = reserve_results
+        self.trader_results = trader_results
+        self.offer_results = offer_results
+        self.branch_results = branch_results
 
-    def _load_data(self):
+        if folder:
+            self.folder = folder
+            self._load_data(**kargs)
+
+
+
+    def _load_data(self, island=False, summary=False, system=False,
+        bus=False, reserve=False, trader=False, offer=False, branch=False,
+        node=False):
         """
         Load all of the vSPD data from the given folder.
-        If possible pass the folder as an absolute path to minimise issues
+        If possible pass the folder as an absolute path to minimise issues.
+        It is called once on initiation of the class
 
         Parameters
         ----------
@@ -51,15 +134,24 @@ class vSPUD(object):
         folder_dict = {os.path.splitext(os.path.basename(v))[0].split('_')[1]: v for v in folder_contents}
 
         # Load the data
-        self.island_results = pd.read_csv(folder_dict["IslandResults"], parse_dates=True)
-        self.summary_results = pd.read_csv(folder_dict["SummaryResults"], parse_dates=True)
-        self.system_results = pd.read_csv(folder_dict["SystemResults"], parse_dates=True)
-        self.bus_results = pd.read_csv(folder_dict["BusResults"], parse_dates=True)
-        self.reserve_results = pd.read_csv(folder_dict["ReserveResults"], parse_dates=True)
-        self.trader_results = pd.read_csv(folder_dict["TraderResults"], parse_dates=True)
-        self.node_results = pd.read_csv(folder_dict["NodeResults"], parse_dates=True)
-        self.offer_results = pd.read_csv(folder_dict["OfferResults"], parse_dates=True)
-        self.branch_results = pd.read_csv(folder_dict["BranchResults"], parse_dates=True)
+        if island:
+            self.island_results = pd.read_csv(folder_dict["IslandResults"], parse_dates=True)
+        if summary:
+            self.summary_results = pd.read_csv(folder_dict["SummaryResults"], parse_dates=True)
+        if system:
+            self.system_results = pd.read_csv(folder_dict["SystemResults"], parse_dates=True)
+        if bus:
+            self.bus_results = pd.read_csv(folder_dict["BusResults"], parse_dates=True)
+        if reserve:
+            self.reserve_results = pd.read_csv(folder_dict["ReserveResults"], parse_dates=True)
+        if trader:
+            self.trader_results = pd.read_csv(folder_dict["TraderResults"], parse_dates=True)
+        if node:
+            self.node_results = pd.read_csv(folder_dict["NodeResults"], parse_dates=True)
+        if offer:
+            self.offer_results = pd.read_csv(folder_dict["OfferResults"], parse_dates=True)
+        if branch:
+            self.branch_results = pd.read_csv(folder_dict["BranchResults"], parse_dates=True)
 
 
 
