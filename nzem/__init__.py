e64f83791dfdd7ccd904e4114fe9a1df7d5ba3cf
Nigel Cleland <nigel.cleland@gmail.com>
2013-08-29 10:36:43 +1200

    Updated imports for new functionality

--- nzem/__init__.py
+++ nzem/__init__.py
@@ -6,11 +6,13 @@ import wits.wits
 import plotting
 import frequent_io
 import analysis
+import vspd
 
 # Class Imports
 from gnash.gnasher import Gnasher
 from offers.offer_frames import ILOffer, ReserveOffer, EnergyOffer, PLSROffer
 from wits.wits import WitsScraper
-from offers.offer_io import offer_from_file
+from offers.offer_io import offer_from_file, reserve_offer_from_file
+from vspd.vspd import vSPUD_Factory, vSPUD
 
 __version__ = '0.2.1'
